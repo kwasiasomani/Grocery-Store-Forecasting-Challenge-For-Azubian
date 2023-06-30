36d01b525701f11a478345052c6dd61327921f42
Bright Eshun <eshunbright136@gmail.com>
2023-06-30 04:54:56 +0000

    Updated make prediction function to receive pandas dataframe

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,13 @@ import pickle
 from io import StringIO
 from functools import lru_cache
 
+
+def return_features():
+    features_ = ['store_id', 'category_id', 'onpromotion', 'type', 'cluster', 'year', 'month', 'dayofmonth', 'dayofweek', 'dayofyear', 'weekofyear', 'quarter', 'is_month_start', 'is_month_end', 'is_quarter_start', 'is_quarter_end', 'is_year_start', 'is_year_end', 'year_weekofyear', 'x0_Accra', 'x0_Aflao', 'x0_Akim Oda', 'x0_Akwatia', 'x0_Bekwai', 'x0_Cape coast', 'x0_Elmina,', 'x0_Gbawe', 'x0_Ho', 'x0_Hohoe', 'x0_Kintampo', 'x0_Koforidua', 'x0_Kumasi', 'x0_Mampong', 'x0_Obuasi', 'x0_Prestea', 'x0_Suhum', 'x0_Tamale', 'x0_Techiman', 'x0_Tema', 'x0_Teshie', 'x0_Winneba']
+    return features_
+
+
+
 @lru_cache(maxsize=100, )
 def load_file(filename):
     with open(filename, 'rb') as file: # read file
@@ -26,6 +33,8 @@ def date_extracts(df):
     df['is_quarter_end'] = df['date_'].dt.is_quarter_end.astype(int)
     df['is_year_start'] = df['date_'].dt.is_year_start.astype(int)
     df['is_year_end'] = df['date_'].dt.is_year_end.astype(int)
+    df['year_weekofyear'] = ((df['year'] -2017) *100 )+ df['weekofyear']
+
     df.drop(columns=['date_'], inplace=True)
 
 
@@ -33,9 +42,12 @@ def date_extracts(df):
 
 # defining categories and numeric columns
 def make_predcition(Encoder, model, input_df):
+    if isinstance(input_df, dict):
+        # Put the input dictionary in a dataset
+        input_data = pd.DataFrame(input_df)
     
- # Put the input dictionary in a dataset
-    input_data = pd.DataFrame(input_df)
+    if isinstance(input_df, pd.DataFrame):
+        input_data = input_df
     col = ['city']
     columns = list(input_data.columns)
     encoded_cat = Encoder.transform(input_data[col])
@@ -43,10 +55,13 @@ def make_predcition(Encoder, model, input_df):
     encoded_cat_ = pd.DataFrame(encoded_cat, columns=encoded_cols)
 
 
+
     
     # we dropped the categorical encoder column before we concat 
     train_enc = input_data.drop(['city'],axis = 1)
     input_d = pd.concat([train_enc, encoded_cat_], axis=1)
+    input_d = input_d.reindex(columns=return_features())
+
     # print(input_d)
 
     # # convert input_data to a numpy array before flattening to convert it back to a 2D array
